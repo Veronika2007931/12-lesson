5f81cc1d93dc8e990209156cf6ec99e9fe204483
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-08-25 23:58:11 +0300

    add

--- 12hw.js
+++ 12hw.js
@@ -1,46 +1,49 @@
-function delayedPromise(value, delay){
-  return new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-      resolve(value)
-      }, delay)
-  })
-}
+// function delayedPromise(value, delay){
+//   return new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//       resolve(value)
+//       }, delay)
+//   })
+// }
 
+// const arr = [
+//     delayedPromise("hello", 2000),
 
-// delayedPromise(300, 2000).then(succes => console.log(succes))
-
-const arr = [
-    delayedPromise("hello", 2000),
-
-    delayedPromise("hello", 4000), 
+//     delayedPromise("hello", 4000), 
     
-    delayedPromise('hello', 3000),
+//     delayedPromise('hello', 3000),
     
-    delayedPromise("Result", 5000),
+//     delayedPromise("Result", 5000),
     
-    delayedPromise("How are you", 3000),
-]
+//     delayedPromise("How are you", 3000),
+// ]
 
-Promise.all(arr)
-.then(value => console.log(value))
+// Promise.all(arr)
+// .then(value => console.log(value))
 
 
 // №2
-// function randomDelay(value){
-//     const min = 1000
-//     const max = 5000
-// const number = Math.floor(Math.random() * (max - min + 1) + min)
+function randomDelay(value){
+    const min = 1000
+    const max = 5000
+const delay = Math.floor(Math.random() * (max - min + 1) + min)
 
-// new Promise((resolve, reject)=>{
-//     setTimeout(()=>{
-//        resolve(value)
-//     }, number)
+return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+       resolve(value)
+    }, delay)
 
-    
-// })
-// }
+  })
+}
+const arr2 = [
+  randomDelay("hello"),
+  randomDelay("good buy"),
+  randomDelay("how are you"),
+  randomDelay("Where are you"),
+  randomDelay("How old are you")
+]
 
-
-
+Promise.race(arr2)
+.then(succes => console.log(succes)) 
 
 
